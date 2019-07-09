403cd78308a7c63f462ac3b3d498a3df09cb583e
CaroBarrios <52497122+CaroBarrios@users.noreply.github.com>
2019-07-08 22:38:51 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,16 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+edad = parseInt (document.getElementById ("edad").value);
 
-}//FIN DE LA FUNCIÓN
+if (edad == 15)
+
+{
+alert ("Niña bonita");
+}
+
+}
+
+//FIN DE LA FUNCIÓN
